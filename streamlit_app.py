7e596fffac28c549186fe3285f24cdf44c3634a0
anau-snwflk <118990660+anau-snwflk@users.noreply.github.com>
2022-12-01 10:42:09 +0100

    Update streamlit_app.py
    add fruit textbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,10 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
+# add some fruit
+add_my_fruit = streamlit.text_input("What fruit would you like to add?", "jackfruit")
+streamlit.write("Thanks for adding", add_my_fruit)
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
